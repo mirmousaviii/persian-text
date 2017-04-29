f5b5ff973c9f4584708b3b3de483b5198d037e0b
Mostafa Mirmousavi <mirmousavi.m@gmail.com>
2017-04-29 10:53:45 +0430

    Persian numbers

---
+++ persian-text.js
@@ -0,0 +1,17 @@
+function persianNumbers(str) {
+    let enums = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+    let pnums = ['۰', '۱', '۲', '۳', '۴', '۵', '۶', '۷', '۸', '۹'];
+
+    return replaceChar(enums, pnums, str);
+}
+
+function replaceChar(search, replace, subject) {
+    let length = search.length;
+    subject = subject.toString();
+
+    for (let i = 0; i < length; i++) {
+        subject = subject.split(search[i]).join(replace[i]);
+    }
+
+    return subject;
+}
